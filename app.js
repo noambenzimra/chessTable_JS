1ecb1f11a3789e3becc78b100a51aa2d14a46633
noambenzimra <noambenzimra@gmail.com>
2022-04-18 18:31:44 +0300

    i did some updates about the name of the pictures and tried to select the future moves of the player... not there yet but getting there

--- app.js
+++ app.js
@@ -1,323 +1,255 @@
+let clicked = false;
 
-let clicked=false;
-function mikoum(tr,td,i,j){
-    
-    //img.src="images/Noir/2N.svg";
-    let img=document.createElement("img");
+const BOARD_SIZE = 8;
+const WHITE_TYPE = "white";
+const DARK_TYPE = "dark";
 
-    if(i===0){
-            switch(j){
-               
+function addImage(cell, type, name) {
+  const image = document.createElement("img");
+  image.src = "images/" + type + "/" + name + ".svg";
+  cell.appendChild(image);
+}
 
-                case 0:{
-                    console.log(i)
-                    
-                    img.src="images/Noir/firstN.svg";
-                    break;
-                    }
-                
-                 case 1:{
-         
-                    img.src="images/Noir/2N.svg";
-                    break;
-                    }
-                    case 2: {
-                        
-                    img.src="images/Noir/3N.svg";
-                    break;
-                    }
-                    
-                    case 3:{
-                        
-                    img.src="images/Noir/4N.svg";
-                    break;
-                    }
-            
-                    case 4:{
-                        
-                        img.src="images/Noir/5N.svg";
-                        break;
-                    }
-            
-                    case 5:{
-                        
-                         img.src="images/Noir/6N.svg";
-                         break;
-                        }
-                    case 6:{
-                         
-                         img.src="images/Noir/7N.svg";
-                         break;
-                        }
-            
-                        case 7:{
-                         
-                            img.src="images/Noir/8N.svg";
-                            break;
-                           }
+function addImageByIndex(cell, type, index) {
+  if (index === 0 || index === 7) {
+    addImage(cell, type, "rook");
+  } else if (index === 1 || index === 6) {
+    addImage(cell, type, "knight");
+  } else if (index === 2 || index === 5) {
+    addImage(cell, type, "bishop");
+  } else if (index === 3) {
+    addImage(cell, type, "king");
+  } else if (index === 4) {
+    addImage(cell, type, "queen");
+  }
+}
 
-                   }//switch j closing
+let selectedCell = undefined;
 
+// function search(selectedcell){
+//     let player=[]
+//     let td=document.getElementsByTagName("td")
+//     for(let i=0;i<8;i++){
+//     for(let j=0;j<8;j++){
+//         if(selectedcell===td[j]&&td.id==search1(selectedCell)){
+//          let id=td.col;
+//          let number=parseInt(id,10);
+//          let img=td.img;
+//           player.push(new Piece(i,j,"knight",WHITE_TYPE))
+//           return player;
+//           //return player;
 
-                }//case 0 closing
+//         }
 
+//         }
+//     }
+// }
 
-                if(i===1){
-                
-                switch(j){
+// function search1(selectedcell){
+//     let player=[]
+//     let td=document.getElementsByTagName("td")
 
-                    case 0:{
-                             
-                        img.src="images/Noir/9N.svg";
-                        break;}
-         
-                        case 1:{
-                      
-                         img.src="images/Noir/10N.svg";
-                         break;
-                        }   
-                        case 2:{
-                      
-                         img.src="images/Noir/11N.svg";
-                         break;
-                        }   
-                        case 3:{
-                      
-                         img.src="images/Noir/12N.svg";
-                         break;
-                        }   
-                        case 4:{
-                      
-                         img.src="images/Noir/13N.svg";
-                         break;
-                        }   
-                        case 5:{
-                      
-                         img.src="images/Noir/14N.svg";
-                         break;
-                        }   
-                        case 6:{
-                      
-                         img.src="images/Noir/15N.svg";
-                         break;
-                        }   
-                        case 7:{
-                      
-                         img.src="images/Noir/16N.svg";
-                         break;
-                        }
-    
-    
-                }//switch j2 closing
+//     for(let j=0;j<8;j++){
+//         if(selectedcell===td[j]){
+//           return td.id;
+//         }
 
+//         }
 
-             }
+// }
+function onCellClick(e) {
+  let td = document.getElementsByTagName("td");
+  //console.log(td[selectedCell])
+  if (selectedCell !== undefined) {
+    selectedCell.classList.remove("onIt");
+  }
+  selectedCell = e.currentTarget;
+  //console.log(selectedCell);
+  selectedCell.classList.add("onIt");
+  //let x=search(e.currentTarget);
+  // let player=search(selectedCell);
+  //console.log(player[0].getRow()+" "+player[0].getCol())
+  //  ((player[0].getCol())).classList.add('onIt')
+}
 
-             if(i===6){
+// function rules(e){
+//     =e.currentTarget;
+//     if(Piece.type==="pawn")
+//     {
+//         table.rows[Piece.row+1].cells[Piece.col].classList.add('onIt');
+//     }
 
-                switch(j){
+// }
+let pieces = [];
 
-                    case 0:{
-                             
-                        img.src="images/Blanc/9B.svg";
-                        break;}
-         
-                        case 1:{
-                      
-                            img.src="images/Blanc/10B.svg";
-                         break;
-                        }   
-                        case 2:{
-                      
-                            img.src="images/Blanc/11B.svg";
-                         break;
-                        }   
-                        case 3:{
-                      
-                            img.src="images/Blanc/12B.svg";
-                         break;
-                        }   
-                        case 4:{
-                      
-                         img.src="images/Blanc/13B.svg";
-                         break;
-                        }   
-                        case 5:{
-                      
-                         img.src="images/Blanc/14B.svg";
-                         break;
-                        }   
-                        case 6:{
-                      
-                         img.src="images/Blanc/15B.svg";
-                         break;
-                        }   
-                        case 7:{
-                      
-                         img.src="images/Blanc/16B.svg";
-                         break;
-                        }
+// const obj1={
+//     1:"rook",2:"knight",3:"bishop",4:"queen",5:"king",6:"bishop",7:"knight",8:"rook"
 
-             }
-            }
+// }
 
-            
-            if(i===7){
+class Piece {
+  constructor(row, col, type, player) {
+    this.row = row;
+    this.col = col;
+    this.type = type;
+    this.player = player;
+  }
 
-                switch(j){
+  getPlayer() {
+    return this.player;
+  }
 
-                    case 0:{
-                             
-                        img.src="images/Blanc/1B.svg";
-                        break;}
-         
-                        case 1:{
-                      
-                            img.src="images/Blanc/2B.svg";
-                         break;
-                        }   
-                        case 2:{
-                      
-                            img.src="images/Blanc/3B.svg";
-                         break;
-                        }   
-                        case 3:{
-                      
-                            img.src="images/Blanc/4B.svg";
-                         break;
-                        }   
-                        case 4:{
-                      
-                         img.src="images/Blanc/5B.svg";
-                         break;
-                        }   
-                        case 5:{
-                      
-                         img.src="images/Blanc/6B.svg";
-                         break;
-                        }   
-                        case 6:{
-                      
-                         img.src="images/Blanc/7B.svg";
-                         break;
-                        }   
-                        case 7:{
-                      
-                         img.src="images/Blanc/8B.svg";
-                         break;
-                        }
-
-             }
-            }
-
-        //case 1 closing
-
-        //   default:{
-        //         img.src="";
-        //         break;
-
-        //          }
-
-        
-        
-
-td.appendChild(img);
-}//func closing
-
-
-
- window.addEventListener('load', () => {
-
-
-    let body=document.getElementsByTagName("body")[0];
-    
-    let h1=document.createElement("h1");
-    let text2=document.createTextNode("Chess Board Of Noam Benzimra");
-    h1.appendChild(text2);
-    body.appendChild(h1)
-    
-
-
-    let table = document.createElement("table");
-
-    let tblBody = document.createElement("tbody");
-    let thead=document.createElement("thead");
-
-    
-    for(let i=0;i<8;i++){
-    let tr=document.createElement("tr");
-    let th=document.createElement("th");
-    let text=document.createTextNode(String.fromCharCode(97 + i));
-    th.appendChild(text);
-   
-   // tr.appendChild(img.src);
-    for(let j=0;j<8;j++){
-
-     
-    let td=document.createElement("td");
-    tr.appendChild(td);
-    tr.appendChild(th);
-    if((i+j)%2==0){
-            
-        td.className ="black";
-         }
-       else{
-        
-        td.className= "white";
-        } 
-
-    // td.click=addEventListener("click",() => {changeColor(td)});
-     td.addEventListener("click",() =>{
-     table=document.getElementsByTagName("table")[0];
-     let v;
-     table.addEventListener("click",(e)=>{
-     const target=e.target;
-     if(!target.closest("td"))
-     return
-     if(v)
-     {
-   v.classList.remove('onIt');
-    v=undefined;
+  getRow() {
+    return this.row;
+  }
+  getCol() {
+    return this.col;
+  }
+  getPossibleMoves() {
+    let result = [];
+    if (this.type === "pawn") {
+      //   const posibbleMoves = this.getPawnMoves();
+      const posibbleMoves = [1,0];
+      //console.log("hello" + possibleMoves);
+      for (let i = 0; i < posibbleMoves; i++) {
+          this.getRow()+=1;
+       // posibbleMoves[i][0] += this.row;
+        //posibbleMoves[i][1] += this.row;
+      }
+      
     }
-     target.classList.add('onIt');
-     v=target;
-    })}) 
+    return this.getRow();
+  }
 
-   
+  getPawnMoves() {
+    return [3];
+  }
+}
+function getInitialBoard() {
+  let result = [];
+  result.push(new Piece(0, 0, "rook", WHITE_TYPE));
+  result.push(new Piece(0, 1, "knight", WHITE_TYPE));
+  result.push(new Piece(0, 2, "bishop", WHITE_TYPE));
+  result.push(new Piece(0, 3, "king", WHITE_TYPE));
+  result.push(new Piece(0, 4, "queen", WHITE_TYPE));
+  result.push(new Piece(0, 5, "bishop", WHITE_TYPE));
+  result.push(new Piece(0, 6, "knight", WHITE_TYPE));
+  result.push(new Piece(0, 7, "rook", WHITE_TYPE));
+  for (let i = 0; i < 8; i++) {
+    result.push(new Piece(1, i, "pawn", WHITE_TYPE));
+  }
 
-   
-    //td.classList.add("onIt")
-    mikoum(tr,td,i,j);
-    
+  result.push(new Piece(7, 0, "rook", DARK_TYPE));
+  result.push(new Piece(7, 1, "knight", DARK_TYPE));
+  result.push(new Piece(7, 2, "bishop", DARK_TYPE));
+  result.push(new Piece(7, 3, "king", DARK_TYPE));
+  result.push(new Piece(7, 4, "queen", DARK_TYPE));
+  result.push(new Piece(7, 5, "bishop", DARK_TYPE));
+  result.push(new Piece(7, 6, "knight", DARK_TYPE));
+  result.push(new Piece(7, 7, "rook", DARK_TYPE));
+  for (let i = 0; i < 8; i++) {
+    result.push(new Piece(6, i, "pawn", DARK_TYPE));
+  }
+  return result;
+}
 
+window.addEventListener("load", () => {
+  let body = document.getElementsByTagName("body")[0];
+
+  let h1 = document.createElement("h1");
+  let text2 = document.createTextNode("Chess Board Of Noam Benzimra");
+  h1.appendChild(text2);
+  body.appendChild(h1);
+
+  let table = document.createElement("table");
+
+  let tblBody = document.createElement("tbody");
+  let thead = document.createElement("thead");
+
+  for (let i = 0; i < 8; i++) {
+    let tr = document.createElement("tr");
+    let th = document.createElement("th");
+    let text = document.createTextNode(String.fromCharCode(97 + i));
+    th.appendChild(text);
+
+    // tr.appendChild(img.src);
+    for (let j = 0; j < 8; j++) {
+      let td = document.createElement("td");
+      tr.appendChild(td);
+      tr.appendChild(th);
+      //let x=i,y=j
+      td.setAttribute("id", j);
+      if ((i + j) % 2 == 0) {
+        td.className = "black";
+      } else {
+        td.className = "white";
+      }
+
+      //console.log(table.rows[Piece.row].cells[Piece.cell]);
+      //console.log("hello")
+
+      // if (i === 0) {
+      //     addImageByIndex(td, WHITE_TYPE, j);
+      //   } else if (i === 1) {
+      //     addImage(td, WHITE_TYPE, 'pawn');
+      //   } else if (i === 6) {
+      //     addImage(td, DARK_TYPE, 'pawn');
+      //   } else if (i === 7) {
+      //     addImageByIndex(td, DARK_TYPE, j);
+      //   }
+
+      td.addEventListener("click", onCellClick);
+      // td.addEventListener('click', rules)
+
+      // --------------------------------------------------------
+      //  td.addEventListener("click",() =>{
+      //  table=document.getElementsByTagName("table")[0];
+      //  let v;
+      //  table.addEventListener("click",(e)=>{
+      //  const target=e.target;
+      //  if(!target.closest("td"))
+      //  return
+      //   if(v)
+      //  {
+      //  v.classList.remove('onIt');
+      // v=undefined;
+      // }
+      //  target.classList.add('onIt');
+      //  v=target;
+      // })})
+      // /-------------------------------------------------------
+      // mikoum(tr,td,i,j);
     }
     tblBody.appendChild(tr);
     thead.appendChild(th);
-    }
-    table.appendChild(thead);
-     table.appendChild(tblBody);
-     body.appendChild(table);
-     table.setAttribute("border", "4");
+  }
+  table.appendChild(thead);
+  table.appendChild(tblBody);
+  body.appendChild(table);
+  table.setAttribute("border", "4");
 
-    //  td.addEventListener('click', () => {
+  let pieces = getInitialBoard();
+  console.log(pieces[1].getPossibleMoves());
+  for (let piece of pieces) {
+    addImage(
+      tblBody.rows[piece.row].cells[piece.col],
+      piece.player,
+      piece.type
+    );
+  }
 
-        
+  //   const x=[...document.getElementsByTagName("td")].forEach(td => td.addEventListener("click", e => e.target.classList.add ('onIt')));
+  // [...document.getElementsByTagName("td")].forEach(td => td.removeEventListener("click", e => e.target.classList.add ('onIt')));
 
-//   const x=[...document.getElementsByTagName("td")].forEach(td => td.addEventListener("click", e => e.target.classList.add ('onIt')));
-// [...document.getElementsByTagName("td")].forEach(td => td.removeEventListener("click", e => e.target.classList.add ('onIt')));
+  // table.rows[i].cells[j].addEventListener("click",e => e.target.style.background= "#b88474");
 
-// table.rows[i].cells[j].addEventListener("click",e => e.target.style.background= "#b88474");
-
-    //  })
+  //  })
 });
 
-
-
 //     tr=document.getElementsByTagName("tr");
-//     td = document.getElementsByTagName("td"); 
-    
+//     td = document.getElementsByTagName("td");
 
-//      if(!clicked){   
+//      if(!clicked){
 //     table.rows[i].cells[j].classList.add('onIt');
 //     clicked=true;
 //     console.log(x)
@@ -327,14 +259,12 @@ td.appendChild(img);
 //         table.rows[i].cells[j].classList.add('onIt');
 //     }
 
-     
 // }
 
 // function removeColor(i,j){
 //     table.rows[i].cells[j].classList.remove('onIt');
 // }
-    
-    
+
 //     let i=
 //     if(!clicked){
 //         return [...document.getElementsByTagName("td")].forEach(element => element.addEventListener("click", e => e.target.classList.add ('onIt')));
@@ -342,12 +272,3 @@ td.appendChild(img);
 
 //     }
 //     else if()
-
- 
-
-
-
-
-
-
-   
